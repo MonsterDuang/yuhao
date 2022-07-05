c1f333a1f7b08e39a7089fd05a239bf116041941
luozhiyong <monsterluo1119@yeah.net>
2022-07-05 10:37:03 +0800

    ++

--- applet/app.js
+++ applet/app.js
@@ -4,10 +4,6 @@ App({
   utils,
   onLaunch() {
     // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
     // 登录
     wx.login({
       success: res => {
--- applet/pages/index/index.js
+++ applet/pages/index/index.js
@@ -9,7 +9,7 @@ Page({
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    canIUseOpenData: false
   },
   // 事件处理函数
   bindViewTap() {
@@ -37,9 +37,10 @@ Page({
   },
   getUserInfo(e) {
     // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
+    const uinfo = e.detail.userInfo;
+    app.globalData.userInfo = uinfo;
     this.setData({
-      userInfo: e.detail.userInfo,
+      userInfo: uinfo,
       hasUserInfo: true
     })
   }
